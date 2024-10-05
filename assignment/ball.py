f1abe6d6634e50acda2a8e09d65a93f010b3c387
iiakshat <jakshat569@gmail.com>
2024-10-05 19:41:33 +0530

    Create ball.py
    -Tracks the player with the ball

---
+++ assignment/ball.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append("../")
+from utils import bbox_center, measure_distance
+
+class BallAssigner():
+    def __init__(self):
+        self.distance = 70
+
+    def assign_ball(self, players, bbox):
+        
+        min_dist = float("inf")
+        playerwithball = None
+        ball = bbox_center(bbox)
+
+        for playerid, player in players.items():
+            playerbbox = player["bbox"]
+
+            left = measure_distance((playerbbox[0], playerbbox[-1]), ball)
+            right = measure_distance((playerbbox[2], playerbbox[-1]), ball)
+            distance = min(left, right)
+            
+            if distance < self.distance:
+                if distance < min_dist:
+                    min_dist = distance
+                    playerwithball = playerid
+        
+        return playerwithball
