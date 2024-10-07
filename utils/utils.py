498249862d86450969c64f88a71095d933cc8f3c
iiakshat <jakshat569@gmail.com>
2024-10-07 18:15:01 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,34 @@
+import os
 import cv2
+import numpy as np
 
 def read_video(video_path):
+    mainpath = os.path.split(video_path)
+    cache_dir = os.path.join(mainpath[0], 'cache')
+    cache_file = os.path.join(cache_dir, f"{mainpath[-1]}.npz")
+
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+
+    if os.path.exists(cache_file):
+        with np.load(cache_file) as data:
+            frames = [data[f'arr_{i}'] for i in range(len(data.files))]
+        return frames
+
     cap = cv2.VideoCapture(video_path)
     frames = []
+
     while True:
         ret, frame = cap.read()
         if ret:
             frames.append(frame)
         else:
             break
+    cap.release()
+    np.savez_compressed(cache_file, *frames)
+
     return frames
+
 
 def save_video(frames, video_path):
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
