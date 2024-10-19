712e9c5cbaa4f02b64331f1986e993976e34d3f3
iiakshat <jakshat569@gmail.com>
2024-10-19 23:34:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import numpy as np
 from trackers import Tracker
 from utils import read_video, save_video
 from assignment import TeamAssigner, BallAssigner
+import time
 
 def main():
 
+    s = time.perf_counter()
     input_path = 'matches/vid1.mp4'
     output_path = 'output/vid1.avi'
     frames = read_video(input_path)
@@ -42,9 +44,9 @@ def main():
             team_ball.append(team_ball[-1])
         
     team_ball = np.array(team_ball)
-    result  = tracker.draw_annotations(frames, tracks)
+    result  = tracker.draw_annotations(frames, tracks, team_ball)
     save_video(result, output_path)
-
+    print(f"Time: {time.perf_counter() - s}")
 
 if __name__ == '__main__':
     main()
